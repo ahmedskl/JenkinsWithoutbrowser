e578c310495296eea4568afe7457efc3ffcb2411
ahmedskl <shakeel.ahmedeee@gmail.com>
2025-04-15 10:43:12 +0530

    First commit

---
+++ src/test/java/testCases/CreateCourse.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import base.BaseClass;
+
+public class CreateCourse extends BaseClass
+{
+	public void test1()
+	{
+		
+	}
+	public void test2()
+	{
+		
+	}
+	public void test3()
+	{
+		
+	}
+}
