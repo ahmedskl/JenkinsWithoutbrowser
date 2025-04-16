801a951d038717fee558b536b2d84b0004f7e79d
ahmedskl <shakeel.ahmedeee@gmail.com>
2025-04-16 17:39:20 +0530

    new commit

--- src/main/java/browserFactory/BrowserFactory.java
+++ src/main/java/browserFactory/BrowserFactory.java
@@ -13,24 +13,24 @@ import org.openqa.selenium.safari.SafariDriver;
 public class BrowserFactory {
 
 	static WebDriver driver;
-	
+
 	public static WebDriver getBrowserinstance()
 	{
 		return driver;
 	}
 	public static WebDriver startBrowser(String browserName,String applicationUrl) 
 	{
-/*	if we set 3.141.59 jars then java.lang.IllegalStateException: 
- * 	The path to the driver executable must be set by the webdriver.chrome.driver system property.
- */
+		/*	if we set 3.141.59 jars then java.lang.IllegalStateException: 
+		 * 	The path to the driver executable must be set by the webdriver.chrome.driver system property.
+		 */
 		if(browserName.contains("Chrome") || browserName.contains("GC") || browserName.contains("Google Chrome"))
 		{
-		  //	ChromeOptions opt =new ChromeOptions();
-		  //	opt.addArguments("--headless");
-		 //	opt.addArguments("--no-sandbox");
-		 //	driver = new ChromeDriver(opt);
+			//	ChromeOptions opt =new ChromeOptions();
+			//	opt.addArguments("--headless");
+			//	opt.addArguments("--no-sandbox");
+			//	driver = new ChromeDriver(opt);
 			driver = new ChromeDriver();
-			
+
 		}
 		else if(browserName.contains("Edge"))
 		{
@@ -52,10 +52,10 @@ public class BrowserFactory {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));//4.27.0,4.13.0
 		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(20));//4.27.0,4.13.0
-//		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-//		driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+		//		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		//		driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
 		driver.get(applicationUrl);
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //4.27.0,4.13.0jar
 
 		return driver;
